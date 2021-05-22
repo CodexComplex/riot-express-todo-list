38562b3beb80f581facd912aa82e1dee8c64c23f
Hunter Mahlberg <huntermahlberg@gmail.com>
2021-05-22 10:20:34 -0700

    updated IP adress

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.201.103.110',
+      host: '3.80.177.91',
       username: 'ubuntu',
       privateKey: 'Games.pem'
     })
